6f98a11235cb0c7fb72d743af6eab4bda8a54460
apelicier <antoninepelicier10@gmail.com>
2022-03-29 16:48:07 -0400

    first commit

---
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -0,0 +1,111 @@
+package com.codepath.bestsellerlistapp;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.codepath.bestsellerlistapp.models.BestSellerBook;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link BestSellerBook} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ */
+public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<BestSellerBooksRecyclerViewAdapter.BookViewHolder> {
+
+    private final List<BestSellerBook> books;
+    private final OnListFragmentInteractionListener mListener;
+
+    public BestSellerBooksRecyclerViewAdapter(List<BestSellerBook> items, OnListFragmentInteractionListener listener) {
+        books = items;
+        mListener = listener;
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_best_seller_book, parent, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final BookViewHolder holder, int position) {
+        BestSellerBook bestSellerBook = books.get(position);
+        holder.mItem = books.get(position);
+        holder.mBookTitle.setText(books.get(position).title);
+        holder.mBookAuthor.setText(books.get(position).author);
+        holder.buy_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //aad a link to go to the amazon url.
+            }
+        });
+        holder.book_description.setText(books.get(position).description);
+
+        try {
+            String ranking = String.format("%d", bestSellerBook.rank);
+            holder.mBookRanking.setText(ranking);
+            Glide.with(holder.mView).load(bestSellerBook.bookImageUrl).centerInside().into(holder.mbook_image);
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onItemClick(holder.mItem);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mBookTitle;
+        public final TextView mBookAuthor;
+        public BestSellerBook mItem;
+        public TextView mBookRanking;
+        public ImageView mbook_image;
+        public Button buy_button;
+        public TextView book_description;
+
+
+        public BookViewHolder(View view) {
+            super(view);
+            mView = view;
+            mBookTitle = (TextView) view.findViewById(R.id.book_title);
+            mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+            mBookRanking =(TextView) view.findViewById(R.id.ranking);
+            mbook_image = (ImageView)view.findViewById(R.id.mbook_image);
+            buy_button = (Button) view.findViewById(R.id.buy_button);
+            book_description = (TextView) view.findViewById(R.id.book_description);
+        }
+
+        @Override
+        public String toString() {
+            return mBookTitle.toString() + " '" + mBookAuthor.getText() + "'";
+        }
+    }
+}
